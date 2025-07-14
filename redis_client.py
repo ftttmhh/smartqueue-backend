9e2474b9c54789dad14a367574b092126ea77ff6
Firdous Fatimah <firdousfatimah27@gmail.com>
2025-07-14 15:55:40 +0530

    Update redis_client.py

--- redis_client.py
+++ redis_client.py
@@ -1,3 +1,3 @@
 import redis
 
-redis_client = redis.from_url('redis://default:AeJZAAIjcDEzMWNlMDVmOTdlMjU0MDczYTE3ZDljYTVhZDEwMjI0ZHAxMA@touching-goblin-57945.upstash.io:6379', decode_responses=True)
+redis_client = redis.from_url('rediss://default:AeJZAAIjcDEzMWNlMDVmOTdlMjU0MDczYTE3ZDljYTVhZDEwMjI0ZHAxMA@touching-goblin-57945.upstash.io:6379', decode_responses=True)
